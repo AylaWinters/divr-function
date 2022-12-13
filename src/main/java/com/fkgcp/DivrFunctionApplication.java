0b21d84381f10b7068f136f61ba406ca071d1b15
Andre Entrekin <andre.entrekin@gmail.com>
2022-12-13 15:26:02 -0600

    the latest attempt (that doesn't work)

--- src/main/java/com/fkgcp/DivrFunctionApplication.java
+++ src/main/java/com/fkgcp/DivrFunctionApplication.java
@@ -1,10 +1,15 @@
 package com.fkgcp;
 
-import java.util.function.Function;
-
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DivrFunctionApplication {
@@ -14,7 +19,18 @@ public class DivrFunctionApplication {
 	}
 
 	@Bean
-	public Function<String, String> uppercase() {
-	    return value -> value.toUpperCase();
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*");
+
+			}
+		};
 	}
+
+
 }
